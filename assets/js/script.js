0235c97e629c124f5db835d3187008368e50dc7a
Nara19941008 <narminim@code.edu.az>
2022-08-24 00:24:48 +0300

    Added bio table

---
+++ assets/js/script.js
@@ -0,0 +1,132 @@
+//let text="Lorem ipsum"
+
+//let result=text.substring(0,50 + "...")
+
+//span.addEventListener("click", function(){
+   // result="text deleted";
+//})
+
+//let num=66;
+
+//console.log(num.toString())
+
+//console.log(country.length)
+
+//for (const iterator of country) {console.log(iterator);}
+
+//console.log(country[1]);
+
+//console.log(country.toLocaleUpperCase());
+
+//console.log(country.slice(4));
+
+//console.log(country.substring(0,7));
+
+//let country="Azerbaycan";
+
+//let addStr="Respublikasi"
+
+//console.log(country.substr(1,5));
+
+//console.log(country.replace("Azerbaycan","Turkiye"));
+
+//console.log(country.concat(addStr))
+
+//console.log(addStr.trim());
+
+//console.log(country.startsWith("A"));
+
+//if (country.startsWith("A")) {
+  //  console.log("Yes");
+//}else{
+ //   console.log(("No"));
+//}
+
+//console.log(country.charAt(3));
+
+//console.log(country.charCodeAt(3));
+
+// let result=country.split("");
+
+// console.log(result);
+
+// let country="Azerbaycan-Respublikasi-Mudafie-Nazirliyi"
+
+// for (let i = 0; i < country.length; i++) {
+
+//    console.log(country[i]);
+    
+// }
+
+//console.log(country.indexOf("Azerbaycan"));
+
+// function search(str){
+//     if(country.indexOf(str)==-1){
+//         console.log("Not found");
+//         return;
+//     }
+//     console.log("Found");
+// }
+
+//search("Respublika")
+
+// let countryIndex=country.indexOf("Azerbaycan",15);
+
+// let result=country.substring(0,countryIndex) + "...";
+
+// console.log(countryIndex);
+
+// console.log(result);
+
+//let city="Naxcivan";
+
+//console.log(city.includes("y"));
+
+//console.log(Number(False));
+
+// let count="hcbfurbrhgt";
+
+// console.log(typeof count);
+
+// let result=parseInt(count)
+
+// console.log(typeof result);
+
+//let users=["sadiq","Esqin","Nermin","Fidan"];
+
+//console.log(users.toString);
+
+//console.log(users.join("-"));
+
+//users.push("Ramil");
+
+//users.pop();
+
+//users.shift();
+
+//users.unshift("salam")
+
+//users.shift("")
+
+//console.log(users);
+
+//let newArr=users.splice(2,0,"Ayxan")
+
+//let users=["sadiq","Esqin","Nermin","Fidan"];
+
+//console.log(users);
+
+//let result=users.slice(3);
+
+//console.log(result);
+
+// const fruits=["Banana","Apple","Orange","Mango"];
+
+// const citrus=fruits.slice(2,3);
+
+// console.log(citrus);
+
+//const points=[40,100,1,5,25,10];
+// console.log(points);
+// points.sort(function(a,b){return a-b});
+// console.log(points);
